f190dbb7e4fa0ecdc4cebe552a081a32ee2f9282
Jesper Nielsen <jespernielsen1982@gmail.com>
2023-09-04 17:21:14 +0200

    Set up infrastructure.

---
+++ compreq.sh
@@ -0,0 +1,26 @@
+#!/bin/env bash
+
+set -e
+
+git fetch origin main
+git checkout main
+git pull --rebase origin main
+if ! git checkout -b jopfra; then
+    git branch -D jopfra
+    git checkout -b jopfra
+fi
+poetry run python -m requirements
+if [[ $(git status --porcelain) ]]; then
+    poetry update
+    git \
+        -c "user.name=Update requirements bot" \
+        -c "user.email=none" \
+        commit \
+        -am "Update requirements."
+    git push origin +jopfra
+    gh pr create \
+       --title "Update requirements" \
+       --body "Automatic update of requirements." \
+       --reviewer jesnie
+    false
+fi
