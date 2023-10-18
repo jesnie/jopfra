f0896d4eece84b16a9132ba8c1a67b0ee7550c38
Jesper Nielsen <44195043+jesnie@users.noreply.github.com>
2023-10-18 08:27:37 +0200

    Add fetal growth as a problem. (#16)

--- jopfra/problems/__init__.py
+++ jopfra/problems/__init__.py
@@ -1,2 +1,3 @@
+import jopfra.problems.fetal_growth  # pylint: disable=unused-import
 import jopfra.problems.quadratic  # pylint: disable=unused-import
 import jopfra.problems.ssurjano  # pylint: disable=unused-import
