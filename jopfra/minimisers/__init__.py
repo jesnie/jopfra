2e64733a874e7ef21d0b9592545526ef88bfa6ec
Jesper Nielsen <44195043+jesnie@users.noreply.github.com>
2023-10-19 11:13:06 +0200

    Add support for the torch optimizers. (#17)

--- jopfra/minimisers/__init__.py
+++ jopfra/minimisers/__init__.py
@@ -1,2 +1,3 @@
 import jopfra.minimisers.grad_desc  # pylint: disable=unused-import
 import jopfra.minimisers.min  # pylint: disable=unused-import
+import jopfra.minimisers.torch_optim  # pylint: disable=unused-import
